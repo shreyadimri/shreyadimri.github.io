fccc2913b54dbde62ca218572e78bcbcf75613da
Shreya Dimri <shreyadimriofficial@gmail.com>
2025-11-11 23:11:36 +0100

    Header and footer ready, tailwindcss set up, started working on index page

---
+++ src/data/author.ts
@@ -0,0 +1,13 @@
+const author = {
+    name: `Shreya Dimri`,
+    nickname: `Shreya`,
+    email: 'mailto:shreyadimriofficial@gmail.com',
+    github: 'https://github.com/shreyadimri',
+    linkedin: 'https://www.linkedin.com/in/shreya-dimri',
+    bluesky: 'https://bsky.app/profile/shreyadimri.bsky.social',
+    researchgate: 'https://www.researchgate.net/profile/Shreya-Dimri'
+};
+
+
+
+export {author}
---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+// tailwind.config.cjs
+module.exports = {
+content: [
+'./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte,mdx}',
+],
+theme: {
+extend: {},
+},
+plugins: [],
+};
